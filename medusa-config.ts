8508b7805a8687449dabf27630c1801c0ada4974
Avinash Mudunuri <avinashmudunuri@gmail.com>
2024-11-25 00:38:48 +0530

    updating medusa config

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,10 +5,11 @@ loadEnv(process.env.NODE_ENV || "development", process.cwd());
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
-      storeCors: "http://localhost:3000",
-      adminCors: "http://localhost:9000,http://localhost:3000",
-      authCors: "http://localhost:3000,http://localhost:9000",
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
